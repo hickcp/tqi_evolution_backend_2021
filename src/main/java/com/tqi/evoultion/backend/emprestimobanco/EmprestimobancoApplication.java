7f0905ca80667d9389eb07b3d1eb2321810a3ca3
hickcp <henrique_c_p@outlook.com>
2021-12-25 12:13:57 -0300

    inital commit

---
+++ src/main/java/com/tqi/evoultion/backend/emprestimobanco/EmprestimobancoApplication.java
@@ -0,0 +1,13 @@
+package com.tqi.evoultion.backend.emprestimobanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimobancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimobancoApplication.class, args);
+	}
+
+}
