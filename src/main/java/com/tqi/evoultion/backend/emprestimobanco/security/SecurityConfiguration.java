f1192965c9c30f7ae2ec93042c1718ec948f338c
hickcp <henrique_c_p@outlook.com>
2021-12-29 18:41:08 -0300

    login via template removed and cors solucioned

--- src/main/java/com/tqi/evoultion/backend/emprestimobanco/security/SecurityConfiguration.java
+++ src/main/java/com/tqi/evoultion/backend/emprestimobanco/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,7 +20,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll().
+                antMatchers("/usuario/cadastrar").permitAll().
+                anyRequest().authenticated()
+                .and().formLogin().permitAll();
+
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
     }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
